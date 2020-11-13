f6a3a47de38924b0d2ebef40be10a48b0eebc334
shubhamSrivastava93 <shubhamvns115@gmail.com>
2020-11-14 05:05:43 +0530

    minor change

--- app/src/main/java/com/udacity/shoestore/shoelist/ShoeListFragment.kt
+++ app/src/main/java/com/udacity/shoestore/shoelist/ShoeListFragment.kt
@@ -9,7 +9,8 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
-import androidx.navigation.ui.NavigationUI
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.onNavDestinationSelected
 import com.udacity.shoestore.R
 import com.udacity.shoestore.databinding.FragmentShoeListBinding
 import com.udacity.shoestore.models.Shoe
@@ -65,9 +66,7 @@ class ShoeListFragment : Fragment() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return NavigationUI.onNavDestinationSelected(
-            item,
-            view!!.findNavController()
-        ) || super.onOptionsItemSelected(item)
+        return item.onNavDestinationSelected(findNavController())
+                || super.onOptionsItemSelected(item)
     }
 }
